188f84fe2d3362553efd0831f6cc6e1985719c16
nathan-mu <zhaody.mu@gmail.com>
2020-02-24 18:11:33 +1100

    Finish deleting row functions

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {getMovies, getMovie} from "./services/fakeMovieService";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {
+        movies: []
+    }
+
+    handleDeleteClick = (id) => {
+        let movies = [...this.state.movies];
+        const movie = getMovie(id);
+        const index = movies.indexOf(movie);
+        if (index >= 0)
+            movies.splice(index, 1);
+        this.setState({movies});
+    }
+
+    render() {
+        const {movies} = this.state;
+        return (
+            <main className={"container"}>
+                <Header movies={movies}/>
+                <Table movies={movies} handleDelete={this.handleDeleteClick}/>
+            </main>
+        );
+    }
+
+    componentDidMount() {
+        const movies = getMovies();
+        this.setState({movies});
+    }
 }
+
+const Header = (props) => props.movies.length > 0 ? <p>Showing {props.movies.length} movies in the database.</p> :
+    <p>No movies.</p>;
+
+class Table extends Component {
+    render() {
+        const {movies} = this.props;
+        return (
+            movies.length > 0 &&
+            (<table>
+                <tr>
+                    <th>Title</th>
+                    <th>Genre</th>
+                    <th>Stock</th>
+                    <th>Rate</th>
+                </tr>
+                {movies.map(movie => (
+                    <tr>
+                        <td>{movie.title}</td>
+                        <td>{movie.genre.name}</td>
+                        <td>{movie.numberInStock}</td>
+                        <td>{movie.dailyRentalRate}</td>
+                        <td>
+                            <button onClick={() => this.props.handleDelete(movie._id)}>Delete</button>
+                        </td>
+                    </tr>
+                ))}
+            </table>)
+        )
+    }
+}
+
 
 export default App;
